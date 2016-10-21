34608cf4c07643f1b859a8eb747c0a8c356beb8f
Sujaya A Maiyya <sujaya@ResNet-17-15.resnet.ucsb.edu>
2016-10-21 13:55:19 -0700

    Changed main.py to create correlation networks. For every year, create a new network, which is complete graph and each edge's weight will be Pearson's correlation coefficient.

--- code/main.py
+++ code/main.py
@@ -1,5 +1,21 @@
 import pandas as pd
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def plot_correlation_network(corrMatrix):
+	indexes = corrMatrix.index.values
+	G=nx.Graph()
+	G.add_nodes_from(indexes)
+
+	#loop over indexes and for each pair, find its correlation value from corrMatrix and add as weight.
+	for row in indexes:
+		for col in indexes:
+			wt = corrMatrix.get_value(row,col)
+			G.add_edge(row, col, weight=wt )
+	nx.draw(G)
+	#plt.show()
 
 if __name__ == '__main__':
 	df = pd.read_hdf('../data/anes_timeseries_thermometer.h5')
@@ -24,5 +40,9 @@ if __name__ == '__main__':
 		corrMats.append(dataByYear[i].corr(method='pearson'))
 		print('For %d:' % (yearList[i]))
 		print(corrMats[-1])
+		#call function to plot correlation network
+		plot_correlation_network(corrMats[-1])
 		
 	print('NaN values indicate nobody filled out both the questions that year.')
+
+
